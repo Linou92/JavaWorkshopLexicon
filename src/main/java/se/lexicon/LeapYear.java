fc65540235c487e900d638316c52ad172604e06f
Lina Abu Hijleh <lina_abuhijleh@hotmail.com>
2026-06-09 13:32:57 +0200

    Project setup + exercises 1 & 2

--- src/main/java/se/lexicon/LeapYear.java
+++ src/main/java/se/lexicon/LeapYear.java
@@ -1,4 +1,16 @@
 package se.lexicon;
 
+import java.util.Scanner;
+
 public class LeapYear {
+    public static void printLeapYear() {
+        Scanner scanner = new Scanner(System.in);
+        IO.println("Enter a year: ");
+        int year = scanner.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            IO.println(year + " is a leap year.");
+        } else {
+            IO.println(year + " is NOT a leap year.");
+        }
+    }
 }
--- src/main/java/se/lexicon/ProfileCard.java
+++ src/main/java/se/lexicon/ProfileCard.java
@@ -1,4 +1,17 @@
 package se.lexicon;
 
 public class ProfileCard {
+
+    public static void printProfile() {
+        String name = "Lina";
+        int age = 34;
+        String city = "Växjö";
+        IO.println("====================\n" +
+                "My Profile\n" +
+                "====================\n" +
+                "Name : " + name + "\n" +
+                "Age  : " + age + "\n" +
+                "City : " + city + "\n" +
+                "====================");
+    }
 }
